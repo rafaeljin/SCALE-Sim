a86ba2201b7d16f00bbaf9c201df4faa1cf58776
Rafael Jin <rafaeljin1994@gmail.com>
2018-07-25 12:57:09 -0400

    fix deque maxlen error

--- dram_limited.py
+++ dram_limited.py
@@ -1,7 +1,6 @@
 import os.path
 import math
-import queue
-from tqdm import tqdm
+import collections
 
 def prune(input_list):
     l = []
@@ -26,7 +25,7 @@ class FIFO_mem:
     def __init__(self,n):
         self.size = n
         self.elems = set()
-        self.q = queue.Queue(maxsize = n)
+        self.q = collections.deque(maxlen = n)
 
     # check whether i is in memory
     def check(self,i):
@@ -40,9 +39,9 @@ class FIFO_mem:
         ret = -1
         if i not in self.elems: # not in mem
             if len(self.elems) >= self.size:
-                ret = self.q.get()
+                ret = self.q.popleft()
                 self.elems.remove(ret)
-            self.q.put(i)
+            self.q.append(i)
             self.elems.add(i)
         return ret
 
@@ -52,13 +51,15 @@ class ideal_mem:
         self.size = n
         self.last_used = {} 
         self.current_cycle = 0
-        self.freelist = queue.Queue(maxsize = n)
+        # deque should not have maxlen
+        self.freelist = collections.deque()
         # list of elements 
         self.l = [] 
 
     def check (self,ele):
         if ele in self.last_used:
-            return self.last_used[ele]
+            return True
+            #return self.last_used[ele]
         else:
             return False
 
@@ -67,49 +68,90 @@ class ideal_mem:
         print self.last_used 
         print ("l",self.l)
         print ("cycle", self.current_cycle)
-        print ("freelist", list(self.freelist.queue))
+        print ("freelist", self.freelist)
         print ""
 
     # elems required at cycle 
     def read (self, elems, cycle_number, max_number_of_reads):
         #print "read"
-        while elems and (elems[-1] in self.last_used or \
-                ( (len(self.last_used) < self.size or self.freelist.qsize() > 0) \
-                and max_number_of_reads > 0) ):
+        # 3 cases: elems[-1] already in memory OR not in memory and memory not full OR not in memory, memory full but free list available
+        # while BASE and (CASE 1 OR (CASE 2 OR CASE 3)), each loop process one element
+        while elems and \
+                (elems[-1] in self.last_used or \
+                        ( (len(self.last_used) < self.size or len(self.freelist) > 0) and max_number_of_reads > 0 ) ):
             # appending execution list for cycle
+            if elems[-1] in self.last_used:
+                print "COND 1"
+            elif max_number_of_reads > 0:
+                if len(self.last_used) < self.size:
+                    print "COND 2"
+                elif len(self.freelist) > 0:
+                    print "COND 3"
+                    print ("check1",self.freelist[0])
+                    if self.freelist[0] not in self.last_used:
+                        print "COND 3 Exception 1"
+                    elif self.last_used[self.freelist[0]] != -1:
+                        print "COND 3 Exception 2"
+                    else:
+                        print "okay"
+                else:
+                    print "COND 4??"
             while cycle_number-self.current_cycle >= len(self.l):
                 self.l.append([])
-            # if not in memory, one quota needed to read 
+            # if not in memory, one read required. 
             if elems[-1] not in self.last_used:
-                max_number_of_reads = max_number_of_reads - 1
-            # memory fully occupied but some can be replaced 
-            if len(self.last_used) == self.size:
-                toremove = self.freelist.get()
-                if self.last_used[toremove] != -1 :
-                    print "tem um erro aqui"
-                else :
-                    del self.last_used[toremove]
-            # regular procedure 
+                max_number_of_reads -= 1
+                # memory fully occupied but some can be replaced 
+                if len(self.last_used) == self.size:
+                    if len(self.freelist) <= 0:
+                        raise Exception("erro aqui")
+                    toremove = self.freelist.popleft()
+                    print ("check2",toremove)
+                    if self.last_used[toremove] != -1 :
+                        raise Exception("tem um erro aqui")
+                    else :
+                        del self.last_used[toremove]
+            # regular procedure: update last_used, append to l and remove from elems
             self.last_used[elems[-1]] = cycle_number
+            if len(self.last_used) > self.size:
+                raise Exception("outro erro")
             self.l[-1].append(elems[-1])
             del elems[-1]
+            # remove fake freelist entries 
+            while len(self.freelist) > 0 and (self.freelist[0] not in self.last_used or (self.last_used[self.freelist[0]] != -1)):
+                self.freelist.popleft()
         #self.debug()
         return max_number_of_reads, elems
 
     def run(self,elems, cycle_number):
         #print "run"
         flag = True
+
+        while len(self.freelist) > 0 and (self.freelist[0] not in self.last_used or (self.last_used[self.freelist[0]] != -1)):
+            raise Exception("exquisito denovo")
+            self.freelist.popleft()
+
         for ele in elems:
             if ele not in self.last_used:
                 flag = False
-        # print ('cn',cycle_number,flag)
+
         if flag and cycle_number == self.current_cycle and len(self.l) > 0:
             for ele in self.l[0]:
                 if self.last_used[ele] == self.current_cycle:
                     self.last_used[ele] = -1
-                    self.freelist.put(ele)
+                    self.freelist.append(ele)
             self.current_cycle = self.current_cycle + 1
             del self.l[0]
+
+        while len(self.freelist) > 0 and (self.freelist[0] not in self.last_used or (self.last_used[self.freelist[0]] != -1)):
+            if self.freelist[0] not in self.last_used:
+                print self.freelist[0]
+                raise Exception("exquisito1")
+            elif self.last_used[self.freelist[0]] != -1:
+                raise Exception("exquisito2")
+            raise Exception("exquisito3")
+            self.freelist.popleft()
+
         #self.debug()
         return flag
 
@@ -174,6 +216,8 @@ def dram_trace_limited_v2(
         dram_trace_file = "dram_log.csv"
     ):
 
+    debug_mode = False 
+
     sram2 = ideal_mem(sram_sz) 
 
     sram_requests = open(sram_trace_file, 'r')
@@ -193,55 +237,47 @@ def dram_trace_limited_v2(
     read_c, run_c = 0, 0
     read_buffer, run_buffer = [], []
 
-    debug_count = 0
-
     while run < len(sram_buffer):
 
-        #print "read"
+        if debug_mode:
+            print "read"
         # read
         read_stall = 0
         max_number_of_reads = max_pure_read_cycles * max_bw
         while True:
-            '''if read_c >= 10019 and read_c <= 10050:
-                print ('read',read,read_c)
-                print ('run',run,run_c)
-                sram2.debug()'''
             if len(read_buffer) == 0:
                 read += 1
                 if read >= len(sram_buffer):
                     break
-                print (read,len(sram_buffer))    
                 read_c, read_buffer = parse_input(sram_buffer[read],min_addr,max_addr)
 
             read_stall += max_number_of_reads
-            print ('1.[]debug_read', read_c, sram2.check(0.0))
             max_number_of_reads,read_buffer = sram2.read(read_buffer,read_c,max_number_of_reads)
-            print ('1.()debug_read', read_c, sram2.check(0.0))
             # sram full
             read_stall -= max_number_of_reads
             # 2 possible break conditions: read buffer not cleared (mem full) 
             # or max pure read cycles used up 
             if len(read_buffer) != 0:
                 break
-
+        if debug_mode:
+            print read_c 
         cycle_stalls = (read_stall+max_bw-1)/max_bw
         stalls += cycle_stalls
         # trace = str(int(run_c)) + " use " + str(int(cycle_stalls)) + "\n"
         # dram.write(trace)
 
-        #print "exe"
+        if debug_mode:
+            print "exe"
         # execute
         while True:
             if run == len(sram_buffer):
                 break
             run_c, run_buffer = parse_input(sram_buffer[run],min_addr,max_addr)
+            # end of one batch
             if len(run_buffer) == 0:
                 run += 1
                 break
-            print ('[]debug_run', run_c, sram2.check(0.0))
-            # move down this lne
             res = sram2.run(run_buffer,run_c)
-            print ('()debug_run', run_c, sram2.check(0.0))
             if not res:
                 dram.write("penalty at " + str(run_c) + "of " + str(penalty) + " cycles\n")
                 stalls += penalty
@@ -255,24 +291,15 @@ def dram_trace_limited_v2(
                     read += 1
                     if read < len(sram_buffer):
                         read_c, read_buffer = parse_input(sram_buffer[read],min_addr,max_addr)
+                    # if read to the end
                     else:
                         break
-                print ('2.[]debug_read', read_c, sram2.check(0.0))
                 number_of_reads, read_buffer = sram2.read(read_buffer,read_c,number_of_reads)
-                print ('2.()debug_read', read_c, sram2.check(0.0))
+                # no more free bandwidth in this cycle
                 if len(read_buffer) != 0:
                     break
-
-        if read_c >= 10019 and read_c <= 10050:
-            print ('read',read,read_c)
-            print ('run',run,run_c)
-            sram2.debug()
-        print ('read',read,read_c)
-        print ('run',run,run_c)
-        if run_c == 10019.0:
-            debug_count = debug_count + 1
-        if debug_count >= 5:
-            break
+        if debug_mode:
+            print run_c
 
     trace = "total stalls: " + str(stalls)
     dram.write(trace)
@@ -280,7 +307,7 @@ def dram_trace_limited_v2(
     return stalls
 
 def dram_trace_limited(
-        sram_sz         = 512 * 1024,
+        sram_szs         = [512 * 1024],
         word_sz_bytes   = 1,
         min_addr = 0, max_addr=1000000,
         max_bws = [1],               
@@ -290,13 +317,14 @@ def dram_trace_limited(
     ):
 
     for max_bw in max_bws:
-        if max_bw == 9:
-            print "max_bw = " + str(max_bw)
+        print "max_bw = " + str(max_bw)
+        for sram_sz in sram_szs:
+            print "\tsram_sz = " + str(sram_sz)
             stalls, max_pure_read_cycles = dram_trace_limited_v1(sram_sz,word_sz_bytes,min_addr,max_addr,max_bw,sram_trace_file,"first_method"+dram_trace_file)
-            print ("first method", stalls)
-            print ("max pure cyles:", max_pure_read_cycles)
+            print "\t\tfirst method " + str(stalls)
+            print "\t\tmax pure cyles:"+ str(max_pure_read_cycles)
             stalls = dram_trace_limited_v2(sram_sz,word_sz_bytes,min_addr, max_addr, max_bw, max_pure_read_cycles, penalty, sram_trace_file,"second_method"+dram_trace_file)
-            print ("second method", stalls)
+            print "\t\tsecond method" + str(stalls)
 
 if __name__ == "__main__":
     t = ideal_mem(5)
